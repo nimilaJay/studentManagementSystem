b647f32836a467b5b42157fc8407497872bd70e9
nimilajay <nimilatj@gmail.com>
2022-07-12 22:47:54 +0530

    Test commit

---
+++ BACKEND/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+})
+
+const connection = mongoose.connection;
+connection.once('open' , () => {
+    console.log("MongoDB connection success!");
+
+})
+
+const studentRouter = require("./routes/students.js");
+
+app.use("/student",studentRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number ${PORT}`)
+})
